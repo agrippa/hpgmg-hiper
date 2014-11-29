92a071f51b2c43f9aba6c0748ebb9df437b83229
Hongzhang Shan <hshan@lbl.gov>
2014-11-29 12:44:12 -0800

    change interpolation_pl.c

--- finite-volume/source/operators/interpolation_pl.c
+++ finite-volume/source/operators/interpolation_pl.c
@@ -79,41 +79,22 @@ void interpolation_pl(level_type * level_f, int id_f, double prescale_f, level_t
   int n;
   int my_tag = (level_f->tag<<4) | 0x7;
 
+#ifdef UPCXX_P2P
+  para_prescale_f[level_f->depth] = prescale_f;
+#endif
 
-
-#ifdef USE_UPCXX
   _timeStart = CycleTime();
+#ifdef USE_UPCXX
+#ifndef UPCXX_P2P
 #ifdef USE_SUBCOMM
   MPI_Barrier(level_f->MPI_COMM_ALLREDUCE);
 #else
   upcxx::barrier();
 #endif
+#endif
+#endif
   _timeEnd = CycleTime();
   level_f->cycles.interpolation_recv += (_timeEnd-_timeStart);
-#elif USE_MPI
-  // by convention, level_f allocates a combined array of requests for both level_f recvs and level_c sends...
-  int nMessages = level_c->interpolation.num_sends + level_f->interpolation.num_recvs;
-  MPI_Request *recv_requests = level_f->interpolation.requests;
-  MPI_Request *send_requests = level_f->interpolation.requests + level_f->interpolation.num_recvs;
-
-  // loop through packed list of MPI receives and prepost Irecv's...
-  _timeStart = CycleTime();
-#ifdef USE_MPI_THREAD_MULTIPLE
-#pragma omp parallel for schedule(dynamic,1)
-#endif
-  for(n=0;n<level_f->interpolation.num_recvs;n++){
-    MPI_Irecv(level_f->interpolation.recv_buffers[n],
-              level_f->interpolation.recv_sizes[n],
-              MPI_DOUBLE,
-              level_f->interpolation.recv_ranks[n],
-              my_tag,
-              MPI_COMM_WORLD,
-              &recv_requests[n]
-    );
-  }
-  _timeEnd = CycleTime();
-  level_f->cycles.interpolation_recv += (_timeEnd-_timeStart);
-#endif
 
   // pack MPI send buffers...
   _timeStart = CycleTime();
@@ -130,21 +111,11 @@ void interpolation_pl(level_type * level_f, int id_f, double prescale_f, level_t
     global_ptr<double> p1, p2;
     p1 = level_c->interpolation.global_send_buffers[n];
     p2 = level_c->interpolation.global_match_buffers[n];
+#ifndef UPCXX_P2P
     upcxx::async_copy(p1, p2, level_c->interpolation.send_sizes[n]);
-  }
-#elif USE_MPI
-#ifdef USE_MPI_THREAD_MULTIPLE
-#pragma omp parallel for schedule(dynamic,1)
+#else
+    sendNbgrDataInt(level_c->interpolation.send_ranks[n], p1, p2, level_c->interpolation.send_sizes[n], level_f->depth, id_f, id_c, level_c->depth);
 #endif
-  for(n=0;n<level_c->interpolation.num_sends;n++){
-    MPI_Isend(level_c->interpolation.send_buffers[n],
-              level_c->interpolation.send_sizes[n],
-              MPI_DOUBLE,
-              level_c->interpolation.send_ranks[n],
-              my_tag,
-              MPI_COMM_WORLD,
-              &send_requests[n]
-    );
   }
 #endif
   _timeEnd = CycleTime();
@@ -158,30 +129,50 @@ void interpolation_pl(level_type * level_f, int id_f, double prescale_f, level_t
   _timeEnd = CycleTime();
   level_f->cycles.interpolation_local += (_timeEnd-_timeStart);
 
-
   // wait for MPI to finish...
   _timeStart = CycleTime();
+
 #ifdef USE_UPCXX
+#ifdef UPCXX_P2P
+
+  int nth = level_f->depth * 20 + id_f;
+  while (1) {
+    int arrived = 0;
+    for (int n = 0; n < level_f->interpolation.num_recvs; n++) {
+      if (level_f->interpolation.flag_data[nth][n]==1) arrived++;
+    }
+    if (arrived == level_f->interpolation.num_recvs) break;
+    upcxx::advance();
+    gasnet_AMPoll();
+  }
+  for (int n = 0; n < level_f->interpolation.num_recvs; n++) {
+    level_f->interpolation.flag_data[nth][n] = 0;
+  }
+
+  syncNeighborInt(level_c->interpolation.num_sends, id_c);
+
+#else
+
   async_copy_fence();
 #ifdef USE_SUBCOMM
   MPI_Barrier(level_f->MPI_COMM_ALLREDUCE);
 #else
   upcxx::barrier();
 #endif
-#elif USE_MPI
-
-  if(nMessages)MPI_Waitall(nMessages,level_f->interpolation.requests,level_f->interpolation.status);
-
+#endif
+#endif
   _timeEnd = CycleTime();
   level_f->cycles.interpolation_wait += (_timeEnd-_timeStart);
-#endif
+
 
   // unpack MPI receive buffers 
+#ifndef UPCXX_P2P
   _timeStart = CycleTime();
   PRAGMA_THREAD_ACROSS_BLOCKS(level_f,buffer,level_f->interpolation.num_blocks[2])
   for(buffer=0;buffer<level_f->interpolation.num_blocks[2];buffer++){IncrementBlock(level_f,id_f,prescale_f,&level_f->interpolation.blocks[2][buffer], NULL, 0);}
   _timeEnd = CycleTime();
   level_f->cycles.interpolation_unpack += (_timeEnd-_timeStart);
+#endif
  
   level_f->cycles.interpolation_total += (uint64_t)(CycleTime()-_timeCommunicationStart);
 }
